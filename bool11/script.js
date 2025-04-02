9e10ff8af443c5856cc518407b6573d1ce0f7f42
Diyor <baxacap@gmail.com>
2025-04-02 16:29:26 +0500

    to bool 13

---
+++ bool11/script.js
@@ -0,0 +1,10 @@
+let a = parseFloat(prompt("Son kiriting"));
+let b = parseFloat(prompt("Son kiriting"));
+
+if (a % 2 === 0 && b % 2 === 0) {
+  console.log(true);
+} else if (a % 2 === 1 && b % 2 === 1) {
+  console.log(true);
+} else {
+  console.log(false);
+}
