730b1894591bbb0515dc049a8823001ccd21e489
Diyor <baxacap@gmail.com>
2025-04-25 19:44:47 +0500

    to bool22

---
+++ bool20/app.js
@@ -0,0 +1,9 @@
+let a = parseFloat(prompt("Son kiriting"));
+let b = parseFloat(prompt("Son kiriting"));
+let c = parseFloat(prompt("Son kiriting"));
+
+if (a == b || a == c || b == c) {
+  console.log(false);
+} else {
+  true;
+}
